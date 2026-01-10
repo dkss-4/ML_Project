4c66b0cd8a714b2f215ec183a0749ccf83c673b5
Deeksha Sharma <dks.deeksha@gmail.com>
2026-01-10 22:39:35 +0530

    Model_Trainer

--- src/utils.py
+++ src/utils.py
@@ -20,20 +20,15 @@ def save_object(file_path, obj):
             pickle.dump(obj, file_obj)
 
     except Exception as e:
-        raise CustomException(e, sys)
+        raise CustomException(e,sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+def evaluate_models(X_train,y_train,X_test,y_test,models):
     try:
         report = {}
-
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            para=param[list(models.keys())[i]]
 
-            gs = GridSearchCV(model,para,cv=3)
-            gs.fit(X_train,y_train)
-
-            model.set_params(**gs.best_params_)
+            
             model.fit(X_train,y_train)
 
             #model.fit(X_train, y_train)  # Train model
@@ -51,12 +46,5 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
         return report
 
     except Exception as e:
-        raise CustomException(e, sys)
+        raise CustomException(e,sys)
     
-def load_object(file_path):
-    try:
-        with open(file_path, "rb") as file_obj:
-            return pickle.load(file_obj)
-
-    except Exception as e:
-        raise CustomException(e, sys)
